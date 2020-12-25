16efe68d005db363c9735823fe86a4cc4d2866ad
igorplopes <igorplopes16@gmail.com>
2020-12-25 11:54:55 -0300

    Adicionado o datepicker

---
+++ components/DateSelector.js
@@ -0,0 +1,11 @@
+import Datetime from "react-datetime";
+
+import React from "react";
+
+export default function DateSelector() {
+  return (
+    <div>
+      <Datetime></Datetime>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import DateSelector from "../components/DateSelector";
 
 export default function Home() {
   return (
     <div className='container'>
       <h1>Analisador de resultados da Mega-Sena</h1>
+      <DateSelector></DateSelector>
     </div>
   );
 }
