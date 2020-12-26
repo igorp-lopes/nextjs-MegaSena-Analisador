66cfb26f2b8ab1d1b4eed36428f16e3fd46f6191
igorplopes <igorplopes16@gmail.com>
2020-12-26 02:01:36 -0300

    Criado componente seletor de data

--- components/DateSelector.js
+++ components/DateSelector.js
@@ -1,11 +1,12 @@
+import Container from "react-bootstrap/Container";
 import Datetime from "react-datetime";
 
 import React from "react";
 
 export default function DateSelector() {
   return (
-    <div>
+    <Container>
       <Datetime></Datetime>
-    </div>
+    </Container>
   );
 }
