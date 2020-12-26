0e53da9117a67e6f257387d1783c68e4869b8af7
igorplopes <igorplopes16@gmail.com>
2020-12-26 02:02:04 -0300

    Inserido datepicker na página principal

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,23 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import DateSelector from "../components/DateSelector";
+
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Home() {
   return (
-    <div className='container'>
+    <Container>
       <h1>Analisador de resultados da Mega-Sena</h1>
-      <DateSelector></DateSelector>
-    </div>
+      <Row>
+        <Col sm={4}>
+          <DateSelector></DateSelector>
+        </Col>
+        <Col>
+          <p>Teste</p>
+        </Col>
+      </Row>
+    </Container>
   );
 }
