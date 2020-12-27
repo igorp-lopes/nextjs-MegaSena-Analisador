68159ff73041f31847c9c0640479e954d7856374
igorp-lopes <igorplopes16@gmail.com>
2020-12-27 10:55:32 -0300

    Atualizada aparência do datepicker com o bootstrap

--- components/DateSelector.js
+++ components/DateSelector.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import moment from "moment";
+import Button from "react-bootstrap/Button";
 
 export default function DateSelector({ date, setDate }) {
   return (
     <DatePicker
       selected={date}
       dateFormat={"dd/MM/yyyy"}
+      customInput={
+        <Button variant='primary'>{date.toLocaleDateString()}</Button>
+      }
       onChange={(selectedDate) => {
         setDate(selectedDate);
       }}
