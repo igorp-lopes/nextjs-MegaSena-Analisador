431016172da8570a87d995d32cca4c4299bcdfe0
igorp-lopes <igorplopes16@gmail.com>
2020-12-27 00:14:01 -0300

    Adicionado state ao datepicker

--- components/DateSelector.js
+++ components/DateSelector.js
@@ -3,15 +3,12 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import moment from "moment";
 
-export default function DateSelector() {
-  const [date, setDate] = useState(new Date());
-
+export default function DateSelector({ date, setDate }) {
   return (
     <DatePicker
       selected={date}
+      dateFormat={"dd/MM/yyyy"}
       onChange={(selectedDate) => {
-        console.log(selectedDate);
-        debugger;
         setDate(selectedDate);
       }}
     ></DatePicker>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,26 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import DateSelector from "../components/DateSelector";
+import React, { useState } from "react";
+
 
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function Home() {
+  const [date, setDate] = useState(new Date());
+
   return (
     <div>
       <Container>
         <h1>Analisador de resultados da Mega-Sena</h1>
         <Row>
           <Col sm={4}>
-            <DateSelector></DateSelector>
+            <DateSelector date={date} setDate={setDate}></DateSelector>
           </Col>
           <Col>
-            <p>Teste</p>
+            <p>{date.toLocaleDateString()}</p>
           </Col>
         </Row>
       </Container>
