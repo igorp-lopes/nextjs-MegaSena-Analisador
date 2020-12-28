87061dd314f0fe27ecd36a57082506c3d8c99d94
igorp-lopes <igorplopes16@gmail.com>
2020-12-28 01:05:42 -0300

    Criado o grupo de botões seletores

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,42 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import DateSelector from "../components/DateSelector";
 import React, { useState } from "react";
-
+import DateSelector from "../components/DateSelector";
+import RadioSelectors from "../components/RadioSelectors.js";
 
 import Container from "react-bootstrap/Container";
+import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 export default function Home() {
   const [date, setDate] = useState(new Date());
+  const [selectedRadio, setSelectedRadio] = useState();
 
   return (
     <div>
       <Container>
-        <h1>Analisador de resultados da Mega-Sena</h1>
-        <Row>
-          <Col sm={4}>
-            <DateSelector date={date} setDate={setDate}></DateSelector>
-          </Col>
-          <Col>
-            <p>{date.toLocaleDateString()}</p>
-          </Col>
-        </Row>
+        <Jumbotron>
+          <h1>Analisador de resultados da Mega-Sena</h1>
+          <Row>
+            <Col>
+              <div>
+                <p>Selecione a data inicial para considerar o filtro</p>
+                <DateSelector date={date} setDate={setDate}></DateSelector>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <br />
+              <p>{date.toLocaleDateString()}</p>
+              <RadioSelectors
+                selectedRadio={selectedRadio}
+                setSelectedRadio={setSelectedRadio}
+              ></RadioSelectors>
+            </Col>
+          </Row>
+        </Jumbotron>
       </Container>
     </div>
   );
