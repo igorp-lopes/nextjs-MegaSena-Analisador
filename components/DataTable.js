b2746c074fa889680378514289c5015f9ff9e9e5
igorp-lopes <igorplopes16@gmail.com>
2020-12-30 16:54:17 -0300

    Criado o componente da tabela de resultados da análise

---
+++ components/DataTable.js
@@ -0,0 +1,13 @@
+import { func } from "prop-types";
+import React from "react";
+import Table from "react-bootstrap/Table";
+
+function DataTable({ tableData }) {
+  return (
+    <div>
+      <Table responsive></Table>
+    </div>
+  );
+}
+
+export default DataTable;
