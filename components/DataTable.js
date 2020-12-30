4b0f84e9dbd21998add7be64b96b342bb3685582
igorp-lopes <igorplopes16@gmail.com>
2020-12-30 16:56:03 -0300

    Criado os componentes do header da tabela

--- components/DataTable.js
+++ components/DataTable.js
@@ -2,10 +2,32 @@ import { func } from "prop-types";
 import React from "react";
 import Table from "react-bootstrap/Table";
 
+function getKeys(tableRow) {
+  return Object.keys(tableRow);
+}
+
+const getTableHeader = function (tableData) {
+  const tableKeys = getKeys(tableData[0]);
+
+  return tableKeys.map((key, index) => {
+    return <th key={index}>{key.toUpperCase()}</th>;
+  });
+};
+
+function TableHeader({ tableData }) {
+  return (
+    <thead>
+      <tr>{getTableHeader(tableData)}</tr>
+    </thead>
+  );
+}
+
 function DataTable({ tableData }) {
   return (
     <div>
-      <Table responsive></Table>
+      <Table responsive>
+        <TableHeader tableData={tableData}></TableHeader>
+      </Table>
     </div>
   );
 }
