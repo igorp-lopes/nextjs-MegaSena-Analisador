fe1ec49f490b2a1cf88167094ee4c7c7e92936d8
igorp-lopes <igorplopes16@gmail.com>
2020-12-30 21:03:23 -0300

    Adicionado JSON para teste das tabelas

--- components/AnalysisCard.js
+++ components/AnalysisCard.js
@@ -2,17 +2,20 @@ import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
+import DataTable from "../components/DataTable";
 
-function AnalysisCard({ category, eventKey }) {
+function AnalysisCard({ category, eventKey, tableData }) {
   return (
     <Card>
       <Card.Header>
-        <Accordion.Toggle as={Button} variant='primary' eventKey={eventKey}>
+        <Accordion.Toggle as={Button} variant='link' eventKey={eventKey}>
           {category}
         </Accordion.Toggle>
       </Card.Header>
       <Accordion.Collapse eventKey={eventKey}>
-        <Card.Body>Hello! I'm the body</Card.Body>
+        <Card.Body>
+          <DataTable tableData={tableData}></DataTable>
+        </Card.Body>
       </Accordion.Collapse>
     </Card>
   );
