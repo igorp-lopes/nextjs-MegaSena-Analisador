83616f5513ab031dfd9a02d6d734f21364fb913e
igorp-lopes <igorplopes16@gmail.com>
2021-01-09 12:33:34 -0300

    Adicionada  a possibilidade de query na requisição

--- helpers/fetchData.js
+++ helpers/fetchData.js
@@ -4,24 +4,36 @@ async function fetchAPIData(url) {
   return jsonData;
 }
 
-export async function fetchOccurrences() {
+export async function fetchOccurrences(query) {
   const url = "http://localhost:8000/occurrences";
+
+  if (query) {
+    url += '?startDate=' + query
+  }
 
   const occurrencesJson = await fetchAPIData(url);
 
   return occurrencesJson;
 }
 
-export async function fetchEarliest() {
+export async function fetchEarliest(query) {
   const url = "http://localhost:8000/dates/earliest";
+
+  if (query) {
+    url += '?startDate=' + query
+  }
 
   const earliestJson = await fetchAPIData(url);
 
   return earliestJson;
 }
 
-export async function fetchOldest() {
+export async function fetchOldest(query) {
   const url = "http://localhost:8000/dates/oldest";
+
+  if (query) {
+    url += '?startDate=' + query
+  }
 
   const oldestJson = await fetchAPIData(url);
 
