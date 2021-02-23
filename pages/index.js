d805fd47fc2b771df3ff3d0187c81999375dd7de
igorp-lopes <igorplopes16@gmail.com>
2021-02-22 22:58:18 -0300

    Divisão da página no componente Body

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import DateSelector from "../components/DateSelector";
 import RadioSelectors from "../components/RadioSelectors.js";
 import AnalysisAccordion from "../components/AnalysisAccordion";
 import TogglingButton from "../components/TogglingButton";
+import Body from "../components/Body";
 
 import Container from "react-bootstrap/Container";
 import Jumbotron from "react-bootstrap/Jumbotron";
@@ -17,41 +18,7 @@ export default function Home() {
 
   return (
     <div>
-      <Container fluid>
-        <Jumbotron>
-          <Row>
-            <Col>
-              <h1>Analisador de resultados da Mega-Sena</h1>
-              <br></br>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <TogglingButton
-                text='Filtrar as análises a partir de uma data específica'
-                toggled={toggled}
-                setToggled={setToggled}
-              ></TogglingButton>
-              {toggled ? (
-                <div>
-                  <p> Selecione uma data para o filtro</p>
-                  <DateSelector date={date} setDate={setDate}></DateSelector>
-                </div>
-              ) : (
-                <div></div>
-              )}
-            </Col>
-          </Row>
-          <br></br>
-          <Row>
-            <Col>
-              <AnalysisAccordion
-                date={toggled && date.toLocaleDateString()}
-              ></AnalysisAccordion>
-            </Col>
-          </Row>
-        </Jumbotron>
-      </Container>
+      <Body></Body>
     </div>
   );
 }
