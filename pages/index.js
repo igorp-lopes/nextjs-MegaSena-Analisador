87a013065c1f6380b2bb3fb0ff1eecae33652a17
igorp-lopes <igorplopes16@gmail.com>
2021-02-22 22:58:42 -0300

    Atualizado o head da página

--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,19 @@ export default function Home() {
 
   return (
     <div>
+      <Head>
+        <title>Analisador da Mega Sena</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='author' content='Igor Peterossi Lopes'></meta>
+        <meta
+          name='description'
+          content='Analisador de resultados da loteria da Mega Sena'
+        ></meta>
+        <meta
+          name='keywords'
+          content='Mega, Sena, Loteria, Análise, Analisador'
+        ></meta>
+      </Head>
       <Body></Body>
     </div>
   );
